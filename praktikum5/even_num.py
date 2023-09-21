30b6b7281b163b5d54e8ac11066e8f8b32c091ff
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-21 12:01:42 +0700

    add praktikum 5

---
+++ praktikum5/even_num.py
@@ -0,0 +1,22 @@
+# Program untuk mencari bilangan ganji
+
+num = int(input("Masukkan maks angka : "))
+
+for i in range(1, num + 1):
+    if i % 2 == 0:
+        continue
+
+    print(i)
+
+
+print("")
+
+
+i = 0
+while i < num:
+    i += 1
+
+    if i % 2 == 0:
+        continue
+
+    print(i)
